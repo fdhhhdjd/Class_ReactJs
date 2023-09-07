6008cbd0ffaa19f83e2b395f4d9541ba2f9b57c7
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-07 14:02:15 +0700

    #1 useState

---
+++ src/hooks/useState/learn_5/categori_item.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Categories = ({ categories, activeCategory, onFilterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            key={index}
+            className={`${
+              activeCategory === category ? "filter-btn active" : "filter-btn"
+            }`}
+            onClick={() => onFilterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/hooks/useState/learn_5/menu.js
@@ -0,0 +1,27 @@
+//* REACT
+import React from "react";
+
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((item, index) => {
+        const { id, title, img, desc, price } = item;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
+  
