37f7ce234bf585f089d2da96e628ba2bdb87cb46
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-07 13:35:02 +0700

    #1 useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from "react";
 import "./App.css";
+
+//* Hook
 import LearnOne from "./hooks/useState/learn_1";
 import LearnTow from "./hooks/useState/learn_2";
 import LearnThree from "./hooks/useState/learn_3";
+import LearnFour from "./hooks/useState/learn_4";
 
 function App() {
   return (
@@ -10,7 +13,8 @@ function App() {
       {/* 1. UseState */}
       {/* <LearnOne /> */}
       {/* <LearnTow /> */}
-      <LearnThree />
+      {/* <LearnThree /> */}
+      {/* <LearnFour /> */}
     </Fragment>
   );
 }
---
+++ src/hooks/useState/learn_4/index.js
@@ -0,0 +1,70 @@
+//* REACT
+import React, { useState, useEffect } from "react";
+
+// LIBRARY
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
+//* DATA
+import { SliderData } from "./data"; 
+
+//* CSS
+import "./Slider.css";
+
+const Slider = () => {
+  const [current, setCurrent] = useState(0);
+  const length = SliderData.length;
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const moveDot = (index) => {
+    setCurrent(index);
+  };
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setCurrent(current === length - 1 ? 0 : current + 1);
+    }, 1000);
+    console.log(current);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [current, length]);
+  return (
+    <>
+      <section className="slider">
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+        {SliderData.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={index === current ? "slide active" : "slide"}
+            >
+              {index === current && (
+                <img
+                  src={item.image}
+                  key={index}
+                  alt="Travel image"
+                  className="image"
+                />
+              )}
+            </div>
+          );
+        })}
+        <div className="container-dots">
+          {Array.from({ length: 5 }).map((item, index) => (
+            <div
+              onClick={() => moveDot(index)}
+              className={current === index ? "dot active" : "dot"}
+            ></div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Slider;
