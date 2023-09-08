7db98d1088aa8f7ca55523f1c86766bbc7dd5ccc
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-08 10:51:30 +0700

    #13 [ Hook ] useCallback

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import UseState from "./hooks/useState/useState";
 import UseEffect from "./hooks/useEffect/useEffect";
 import UseRef from "./hooks/useRef/useRef";
 import UseLayoutEffect from "./hooks/useLayoutEffect/useLayoutEffect";
+import UseCallback from "./hooks/useCallback/useCallback";
 
 ///* HOC
 import Memo from "./Hoc/memo/memo";
 
 function App() {
   return (
-    <Fragment>
+    <Fragment>u
       {/* 1. useState */}
       {/* <UseState /> */}
 
@@ -29,7 +30,10 @@ function App() {
       {/* <UseLayoutEffect/> */}
 
       {/* 5. Memo */}
-      <Memo />
+      {/* <Memo /> */}
+
+      {/* 6.useCallback */}
+      <UseCallback/>
     </Fragment>
   );
 }
---
+++ src/hooks/useCallback/components/content.js
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+
+const Content = ({onIncrement}) => {
+  console.log("re-render-content");
+  return (
+    <>
+      <div>content</div>
+      <button onClick={onIncrement}>Click Me !!!</button>
+    </>
+  );
+};
+
+export default memo(Content);
