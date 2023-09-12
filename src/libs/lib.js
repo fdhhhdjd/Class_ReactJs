5c8697e35ff9c1f98f699767127a2e9a4acf7ab6
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-12 11:35:40 +0700

    #22 React-Redux

--- src/libs/lib.js
+++ src/libs/lib.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from "react";
+
 import Formik from "./Formik";
+import React_Redux from "./redux/react-redux";
 
 const Lib = () => {
   return (
     <Fragment>
       {/* Formik */}
       <Formik />
+
+      {/* Redux */}
+      <React_Redux />
     </Fragment>
   );
 };
---
+++ src/libs/redux/react-redux/index.js
@@ -0,0 +1,26 @@
+//* REACT
+import React from "react";
+
+//* CSS
+import "./style.css";
+
+//* LIB
+import { Provider } from "react-redux";
+
+//* PAGE
+import TodoList from "./pages/TodoList";
+
+//* STORE
+import store from "./stores/Store";
+
+const React_Redux = () => {
+  return (
+    <>
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
+    </>
+  );
+};
+
+export default React_Redux;
