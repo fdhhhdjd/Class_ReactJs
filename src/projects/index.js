80a824d188b0cd6cb7657ba49aa413522f92ca07
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-18 13:38:34 +0700

    #28 Projects

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Project_1 from './10.E-Commerce-Advance-Filtering';
+
+//* PROJECT
+// import Project_10 from './10.E-Commerce-Advance-Filtering';
+// import Project_3 from './3. Meals API Project';
+import Project_5 from './5. Color Toggler';
 
 const Projects = () => {
   return (
     <>
-      <Project_1 />
+      {/* <Project_3 /> */}
+      <Project_5 />
+      {/* <Project_10 /> */}
     </>
   );
 };
