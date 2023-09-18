3e22fa1fa707a2c84e6d733808642bb6bc13d567
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-18 11:38:21 +0700

    #27 Custom useRouter

--- src/custom_hooks/index.js
+++ src/custom_hooks/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 // import Learn_1 from './learn_1/useFetch';
 // import Learn_2 from './learn_2/useDebounce';
 // import Learn_3 from './learn_3/useImageOnload';
+import Learn_4 from './learn_4/useRouter/useRouter';
 
 const CustomHook = () => {
   return (
@@ -11,6 +12,7 @@ const CustomHook = () => {
       {/* <Learn_1 /> */}
       {/* <Learn_2 /> */}
       {/* <Learn_3 /> */}
+      <Learn_4 />
     </>
   );
 };
