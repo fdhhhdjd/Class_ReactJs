8c273c92e370f0f5730291f448e09f5a19ebd7ac
fdhhhdjd <nguyentientai10@gmail.com>
2023-09-21 14:19:08 +0700

    #31 [ Router ] React Router

--- src/routers/Main.js
+++ src/routers/Main.js
@@ -7,6 +7,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Home from './2.Dynamic Routes/Home';
 import About from './2.Dynamic Routes/About';
 import Detail from './2.Dynamic Routes/Detail';
+import NotFound from './3.Route Specificity/NotFound';
 
 const Main = () => {
   return (
@@ -29,6 +30,7 @@ const Main = () => {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/detail/:id" element={<Detail />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </React.Fragment>
